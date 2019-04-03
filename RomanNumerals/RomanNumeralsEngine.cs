224fe961ff4b20913a0212b9fc42b4f02b9175b1
Michelle Beshears <meeshysu@gmail.com>
2019-04-02 21:10:27 -0500

    project set up and text for first numeral works

---
+++ RomanNumerals/RomanNumeralsEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralsEngine
+    {
+        public static List<string> romanNumerals = new List<string>() { "I" };
+        public static List<int> numerals = new List<int>() { 1 };
+
+        public string ToRomanNumeral(int number)
+        {
+            var index = number - 1;
+            return romanNumerals[index];
+        }
+    }
+}
+
